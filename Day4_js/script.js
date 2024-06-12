731b01270ef818c5f8fde61f8eb89f2793b03f9b
Ashish Kumar <ashish93343@gmail.com>
2024-06-12 11:58:05 +0530

    day5 morning JS(array, obj)

--- Day4_js/script.js
+++ Day4_js/script.js
@@ -11,3 +11,55 @@ function abc(){
     console.log(age);
 }
 abc();
+
+// diff btw '===' and "=="
+let a = 10;
+let k = '10'
+if(a == k){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+if(a===k){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+function printhello() {
+    console.log('hello');
+}
+printhello();
+
+//arrow function
+let username= (parms)=>{
+
+    console.log('hii', parms);
+}
+
+console.log(username('ashish'));
+
+let obj = {
+    "name": 'ashish',
+    "id": 23,
+    "city": "patna",
+
+};
+
+// const input = prompt("Enter the property name (e.g., 'name' or 'id'):");
+const input = 'id';
+function work(input) {
+    console.log(obj[input]);  
+}
+
+work(input);
+
+//array
+const arr = ["one", "two", 100];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[2]);
+
