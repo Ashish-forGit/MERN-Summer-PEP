0f96715dd7c1099c4c007093bcac2320578b51ee
Ashish Kumar <ashish93343@gmail.com>
2024-06-12 10:06:21 +0530

    day4 js afternoon

---
+++ Day4_js/script.js
@@ -0,0 +1,13 @@
+console.log("hello js is loosely and dynamicaly typed");
+var age; //decleration
+age = 19; //initialization
+console.log(age);
+
+var age = 45; // reinitialization
+console.log(age);
+
+function abc(){
+    var name= 'ashish'
+    console.log(age);
+}
+abc();
