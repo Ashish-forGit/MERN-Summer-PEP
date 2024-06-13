ce989bef561c5a4c35fb9540e16d3b6ed38efbf9
Ashish Kumar <ashish93343@gmail.com>
2024-06-13 15:57:48 +0530

    upto day6 array function

--- Day6_js/script.js
+++ Day6_js/script.js
@@ -42,26 +42,154 @@
 // }
 
 
-function handleUserName(e) {
-    console.log('Name:', e.target.value);
+// function handleUserName(e) {
+//     console.log('Name:', e.target.value);
     
-}
-function handleUserAge(e) {
-    console.log('Age:', e.target.value);
+// }
+// function handleUserAge(e) {
+//     console.log('useremail:', e.target.value);
     
-}
-function handleSubmit(e) {
-    e.preventDefault();
-    console.dir(e.target);
-    const username = e.target[0].value
-    const userage = e.target[1].value
-    console.log(username, userage);
+// }
+// function handleSubmit(e) {
+//     e.preventDefault();
+//     const arr= e.target;
+//     const username = e.target[0].value
+//     const useremail = e.target[1].value
+//     console.log(username, useremail);
 
-    if (userage>30) {
-        document.body.append("Please enter less age ");
-        
-    } else {
-        document.body.append("validation Successful")
-    }
+//     const form = document.getElementsByTagName('form')[0];
+//     form.style.display='none';
+
+
+//     renderCard(username, useremail)
     
+
+// }
+
+// const renderCard = (n,e)=>{
+//     const root =document.getElementById("root");
+//     root.setAttribute('class', 'card')
+//     root.innerHTML =
+//     `<h3 class="name">Name is: ${n}</h3>
+//     <h3 class="email">Email is: ${e}</h3>`;
+    
+// }
+
+
+
+
+// const showResult = (res) =>{
+//     console.log(res);
+//     const root = document.getElementById('root')
+//     root.innerHTML = res
+
+// }
+// const printPrity = (res) =>{
+//     console.log(res);
+//     const root = document.getElementById('root')
+//     root.style.color ='green';
+//     root.innerHTML = res
+
+// }
+
+// const sum =(a,b)=>{
+//     // let k = a+b;
+    
+//     // showResult(k);
+//     // console.log(k);
+//     const res = a+b;
+//     return res;
+// }
+// const mul =(a,b)=>{
+//     let k = a*b;
+//     console.log(k);
+//     showResult(k);
+// }
+// const sub =(a,b)=>{
+//     let k = a-b;
+//     console.log(k);
+//     showResult(k);
+// }
+
+// showResult(sum(10,20));
+// printPrity(res)
+
+// const showResult = (res) =>{
+//     console.log(res);
+//     const root = document.getElementById('root')
+//     root.innerHTML = res
+
+// }
+// const printPrity = (res) =>{
+//     console.log(res);
+//     const root = document.getElementById('root')
+//     root.style.color ='green';
+//     root.innerHTML = res
+
+// }
+
+// const sum =(a,b, fn)=>{
+    
+//     const res = a+b;
+//     fn(res);
+// }
+
+// const rr = sum(10,20, printPrity);
+
+// const payWithRazorPay = ()=>{
+//     console.log('Payment processingg with rozerpay');
+// }
+// const payWithPaytm = ()=>{
+//     console.log('Payment processingg with Paytm');
+// }
+// const userDetails = (name, age, seat, processPayment)=>{
+//     console.log(`Booking for ${name} whose age is ${age}`);
+    
+
+//     //................................................
+//     if (true) {
+//         processPayment();
+//     } else{
+//         console.log('error');
+//     }
+// }
+// userDetails('ashish', 19, "ind", payWithPaytm )
+
+
+
+// const arr = [1,2,3,4];
+// const arr1 = arr.shift();
+// console.log(arr.shift());
+// console.log(arr1);
+
+// const arr = ["apple","mango","banana"]
+
+// const printValues= (a,b,c)=>{
+//     console.log("value is : ",a);
+//     console.log("index is : ",b);
+//     console.log("...................");
+// }
+
+// // for (let index = 0; index < arr.length; index++) {
+// //     printValues(arr[index],index, arr)
+// // }
+
+// arr.forEach(printValues);
+
+
+const arr = [1,2,3,4]
+// let sum =0;
+// arr.forEach((a) => {
+//  sum = sum +a;
+// })
+// console.log(sum);
+
+const isEven = (x)=>{
+    if(x%2==0) return true;
+    else return false;
 }
+const res = arr.filter(isEven);
+console.log('res',res);
+console.log('array',arr);
+
+
