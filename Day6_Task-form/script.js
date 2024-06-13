1805efc44e8db69b9b26456e8d887b909d55793f
Ashish Kumar <ashish93343@gmail.com>
2024-06-13 17:50:41 +0530

    day 6 task of form

---
+++ Day6_Task-form/script.js
@@ -0,0 +1,54 @@
+document.getElementById('user-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let dob = document.getElementById('dob').value;
+    let profilePicture = document.getElementById('profile-picture').files[0];
+    let phone = document.getElementById('phone').value;
+    let email = document.getElementById('email').value;
+    let address = document.getElementById('address').value;
+
+    let formContainer = document.getElementById('form-container');
+    formContainer.innerHTML = '';
+    formContainer.style.border = '3px solid #ccc';
+    formContainer.style.textAlign = 'left';
+
+    let profilePictureURL = URL.createObjectURL(profilePicture);
+
+    let idCard = document.createElement('div');
+    idCard.className = 'id-card';
+
+    let img = document.createElement('img');
+    img.src = profilePictureURL;
+    img.alt = 'Profile Picture';
+    img.className = 'left-align'; 
+
+    let nameElement = document.createElement('p');
+    nameElement.innerHTML = `<strong>${name}</strong>`;
+    nameElement.className = 'left-align'; 
+
+    let dobElement = document.createElement('p');
+    dobElement.innerText = `Date of Birth: ${dob}`;
+    dobElement.className = 'left-align'; 
+
+    let phoneElement = document.createElement('p');
+    phoneElement.innerText = `Phone: ${phone}`;
+    phoneElement.className = 'left-align'; 
+
+    let emailElement = document.createElement('p');
+    emailElement.innerText = `Email: ${email}`;
+    emailElement.className = 'left-align'; 
+
+    let addressElement = document.createElement('p');
+    addressElement.innerText = `Address: ${address}`;
+    addressElement.className = 'left-align'; 
+
+    idCard.appendChild(img);
+    idCard.appendChild(nameElement);
+    idCard.appendChild(dobElement);
+    idCard.appendChild(phoneElement);
+    idCard.appendChild(emailElement);
+    idCard.appendChild(addressElement);
+
+    formContainer.appendChild(idCard);
+});
---
+++ FormHW/script.js
@@ -0,0 +1,11 @@
+const togglePassword = document.querySelector('#toggle-password');
+        const password = document.querySelector('#p1');
+
+        togglePassword.addEventListener('click', function (e) {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            this.textContent = type === 'password' ? '👁️' : '👁️‍🗨️';
+        });
+
+
+        
