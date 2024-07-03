8f8fb7dff0d7c7a311a8b3ee753648f606935960
Ashish Kumar <ashish93343@gmail.com>
2024-07-03 22:16:17 +0530

    Day19 - Homepage layout - amazon

--- Day19/app.js
+++ Day19/app.js
@@ -1,36 +1,16 @@
 import ReactDOM from "react-dom/client";
-import './globalStyles.css'
-import { IoSearchSharp } from "react-icons/io5";
+import "./globalStyles.css";
+import HomePage from "./src/pages/homePage";
 
 const parent = document.getElementById("root");
 const root = ReactDOM.createRoot(parent);
 
-const HomePage = () => {
-  return (
-    <div className="homepage-root-container">
-      <nav className="homepage-nav">
-        <h4>Amazon.in</h4>
-        <p>address: <br/> LPU University</p>
-        <div className="homepage-search-container">
-            <select></select>
-            <input></input>
-            <button><IoSearchSharp /></button>
-        </div>
-        <h5>Profile</h5>
-        <h5>Cart</h5>
-      </nav>
-      <div className="category-bar">Category bar</div>
-    </div>
-  );
-};
-
 const App = () => {
-  return (
-    <div>
-      <HomePage />
-      
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 };
 
 root.render(<App />);
