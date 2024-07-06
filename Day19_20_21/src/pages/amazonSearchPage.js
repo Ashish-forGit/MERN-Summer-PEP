0398de5beaa882518bbfa71474e7c58ef365a6af
Ashish Kumar <ashish93343@gmail.com>
2024-07-06 19:05:16 +0530

    added produvtInfo page

---
+++ Day19_20_21/src/pages/amazonSearchPage.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import CategoryBar from "../components/CategoryBar";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+
+
+const SearchPage = ({ categories, searchText, setSearchText }) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const navigate = useNavigate();
+    const handleProductInfo = (id) => {
+        navigate(`/search/${id}`);
+    };
+
+    const fetchProducts = async () => {
+        setLoading(true);
+        const res = await fetch(`https://dummyjson.com/products/search?q=${searchText}`);
+        const data = await res.json();
+        setProducts(data.products);
+        setLoading(false);
+        console.log("API called with search text:", searchText);
+    };
+
+    useEffect(() => {      
+        fetchProducts();
+    }, [searchText]);
+
+    useEffect(() => {
+        fetchProducts();    
+    }, []);
+
+    return (
+        <>
+            <Navbar setSearchText={setSearchText} />
+            <CategoryBar categories={categories} />
+            {loading ? (
+                <div className="product-loading">🙏🏻 Please Wait, Products 👜 are Loading...</div>
+            ) : (
+                <div className="products-container">
+                    {products.map((elem) => (
+                        <div key={elem.id} className="product-card" onClick={() => handleProductInfo(elem.id)}>
+                            <img src={elem.thumbnail} alt={elem.title} className="product-image" />
+                            <div className="product-info">
+                                <h2 className="product-title">{elem.title}</h2>
+                                <p className="product-description">{elem.description}</p>
+                                <div className="product-rating">
+                                    {elem.rating} out of 5 stars
+                                    <span className="rating-count">({elem.stock} in stock)</span>
+                                </div>
+                                <p className="product-price">₹{elem.price}</p>
+                                <button className="add-to-cart-button">Add to cart</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
+    );
+};
+
+export default SearchPage;
---
+++ Day19_20_21/src/pages/homePage.js
@@ -0,0 +1,37 @@
+import { IoSearchSharp } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
+import ProductInfoCard from "../components/productInfoCard";
+import Navbar from "../components/navbar";
+import CategoryBar from "../components/categoryBar";
+import { useNavigate } from "react-router-dom";
+
+
+const HomePage = (props) => {
+    const { productInfoCards, categories, setSearchText } = props;
+
+    const navigate = useNavigate();
+    const openSearchPage = ()=>{
+        navigate("/search");
+    }
+
+    return (
+        <div className="homepage-root-container">
+            <Navbar setSearchText={setSearchText} openSearchPage={openSearchPage} />
+            <CategoryBar categories={categories} />
+            <div className="homepage-body">
+                <img
+                    src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL/24/BAU/feb/PC_hero_1_2x_1._CB582889946_.jpg"
+                    className="carousal-image"
+                />
+                <div className="products-cards-container">
+                    {productInfoCards.map((elem) => {
+                        return <ProductInfoCard data={elem} />;
+                    })}
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default HomePage;
